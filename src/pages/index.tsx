e20d27d27cde99dd7e2f3d27104080f1b3933964
TheBoringDude <iamcoderx@gmail.com>
2021-04-07 19:14:01 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '@components/Layout';
+
+export default function Index() {
+  return (
+    <Layout pageTitle="Home | Nextjs, FaunaDB and Auth0">
+      <div className="h-screen flex items-center justify-center">
+        <p>start coding!</p>
+      </div>
+    </Layout>
+  );
+}
