e0e25184115a53debc0903bd14f3fb7f40139950
TheBoringDude <iamcoderx@gmail.com>
2021-04-08 10:52:59 +0800

    implement:: use auth0 rule for user creation to db

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,16 +1,7 @@
-import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-const afterCallback = (req, res, session, state) => {
-  // TODO: check if the user exists in the db
-  return session;
-};
+export default handleAuth();
 
-export default handleAuth({
-  async callback(req, res) {
-    try {
-      await handleCallback(req, res, { afterCallback });
-    } catch (e) {
-      res.status(e.status || 500).end(e.message);
-    }
-  }
-});
+/*
+--> new users will be automatically registered to the db using the auth0 rule
+*/
---
+++ utils/auth0-rules/newuser.js
@@ -0,0 +1,26 @@
+function (user, context, callback) {
+    const fauna = require('faunadb');
+
+    const { query } = fauna;
+    const { Create, Collection, If, Exists, Get, Match, Index, Select } = query;
+
+    const client = new fauna.Client(
+        { domain: 'db.fauna.com', secret: configuration.FAUNA_SERVER_KEY });
+
+    client.query(
+        If(
+            Exists(Match(Index('user_by_id'), user.user_id)),
+        true, 
+        Create(
+            Collection('users'), 
+            {
+                data: {
+                    user: user.user_id,
+                }
+            }
+        )
+        )
+    );
+
+    return callback(null, user, context);
+}
