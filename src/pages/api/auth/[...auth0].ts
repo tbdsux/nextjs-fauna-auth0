f2c1f6f99782042f3e74afb9a98f640e0c67a8ee
TheBoringDude <iamcoderx@gmail.com>
2021-04-23 23:00:32 +0800

    add alternative without using auth0 rules

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,6 +1,16 @@
 import { getSession, handleAuth, handleCallback, handleLogout } from '@auth0/nextjs-auth0';
 import { UserModel } from '@fauna/models/user-model';
 
+/* --> ALTERNATIVE w/out using Auth0 Rules
+const afterCallback = async (req: NextApiRequest, res: NextApiResponse, session, state) => {
+  return await CreateUserIfNotExists(session.user).then(async () => {
+    const token = await obtainFaunaDBToken(session.user.sub);
+    session.user.token = token;
+    return session;
+  });
+};
+*/
+
 const afterCallback = async (req, res, session, state) => {
   const user = new UserModel();
   const token = await user.obtainFaunaDBToken(session.user.sub);
@@ -36,4 +46,8 @@ export default handleAuth({
 
 /*
 --> new users will be automatically registered to the db using the auth0 rule
+*/
+
+/*
+  NOTE: /api/auth/[...auth] -> nextjs-auth0 api router handler
 */
